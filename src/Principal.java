65e76d80ee052e38ab948e3da64360080b4ccf7e
davidtahimalvesbrito <davidtahim@gmail.com>
2026-03-30 21:30:53 -0300

    remove linhas em branco desnecessárias na classe Principal

--- src/Principal.java
+++ src/Principal.java
@@ -6,14 +6,12 @@ import br.com.cdl.moviehub.modelos.Serie;
 public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // Informação dos filmes
         Filme filme1 = new Filme();
         System.out.println("Digite o nome do filme:");
         filme1.setNome(scanner.nextLine());
-        
         System.out.println("Digite o nome do diretor do filme:");
         filme1.setDiretor(scanner.nextLine());
-
-
         System.out.println("Digite o ano de lançamento do filme:");
         filme1.setAnoDeLancamento(scanner.nextInt());
         System.out.println("Digite a duração do filme em minutos:");

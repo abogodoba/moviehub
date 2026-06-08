b0dda4d43894adc9f88e3948e7456738b73c28ef
Jonatha de Araujo <93874031+abogodoba@users.noreply.github.com>
2026-06-08 14:57:24 -0300

    Update Filme.java

--- src/br/com/cdl/moviehub/modelos/Filme.java
+++ src/br/com/cdl/moviehub/modelos/Filme.java
@@ -1,7 +1,15 @@
 package br.com.cdl.moviehub.modelos;
+
 public class Filme extends Titulo {
     private String diretor;
 
+    // MÉTODO PARA ALTERAR O NOME E O DIRETOR DO FILME
+    public void atualizarDados(String novoNome, String novoDiretor) {
+        this.setNome(novoNome); // Altera o nome que vem da classe Titulo
+        this.diretor = novoDiretor; // Altera o diretor aqui do Filme
+    }
+
+    // Getters e Setters
     public String getDiretor() {
         return diretor;
     }
@@ -13,5 +21,13 @@ public class Filme extends Titulo {
     @Override
     public int getDuracaoEmMinutos() {
         return getTotalEmMinutos();
+    }
+    
+    // MÉTODO EXEMPLO: Mostra os dados do filme na tela
+    public void exibirFichaTecnica() {
+        System.out.println("--- DADOS DO FILME ---");
+        System.out.println("Nome do Filme: " + this.getNome());
+        System.out.println("Diretor: " + this.diretor);
+        System.out.println("----------------------");
     }
 }
